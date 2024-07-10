6d2b4a764c46cea7319c06590e69e338cb68a6ac
Cecron <b9kvz7be6v@snkmail.com>
2024-07-10 21:19:12 +0200

    Update bootstrap.sh
    Minimal content in order to test.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 apt update
-apt upgrade --yes
+# apt upgrade --yes
 # apt autoremove --yes
-apt install --yes software-properties-common
-add-apt-repository --yes --update ppa:ansible/ansible
+# apt install --yes software-properties-common
+# add-apt-repository --yes --update ppa:ansible/ansible
 apt update
-apt install --yes --no-install-recommends ansible git
+# apt install --yes --no-install-recommends ansible git
