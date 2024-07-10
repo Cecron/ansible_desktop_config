ce94c60d0397b7d9118800dbddd5cb37744d76e6
Cecron <b9kvz7be6v@snkmail.com>
2024-07-10 21:27:22 +0200

    Show how to run bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
+# run using e.g curl: 
+# curl -s  https://raw.githubusercontent.com/Cecron/ansible_desktop_config/main/bootstrap.sh | sudo bash -s
 apt update
-# apt upgrade --yes
+apt upgrade --yes
 # apt autoremove --yes
-# apt install --yes software-properties-common
-# add-apt-repository --yes --update ppa:ansible/ansible
+apt install --yes software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
 apt update
-# apt install --yes --no-install-recommends ansible git
+apt install --yes --no-install-recommends ansible git
